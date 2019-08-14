40345ee2d2270b790ff88c1c421b2d91835c5bfa
Michael Hegerhorst <michael.hegerhorst@gmail.com>
2019-08-14 02:24:24 -0600

    Added ITileEntityChangesColorRainbow

--- src/main/java/com/themikeste1/wabbits/core/tileentities/TileEntityRainbowBricks.java
+++ src/main/java/com/themikeste1/wabbits/core/tileentities/TileEntityRainbowBricks.java
@@ -1,7 +1,6 @@
 package com.themikeste1.wabbits.core.tileentities;
 
 //META
-import com.themikeste1.wabbits.api.state.properties.BlockStateProperties;
 import com.themikeste1.wabbits.atlas.TileEntities;
 
 //Minecraft
@@ -17,8 +16,7 @@ import net.minecraft.world.World;
  * @see com.themikeste1.wabbits.core.blockitems.BlockItemRainbowBricks
  * @see com.themikeste1.wabbits.atlas.Blocks
  */
-public class TileEntityRainbowBricks extends TileEntity implements ITickableTileEntity {
-
+public class TileEntityRainbowBricks extends TileEntity implements ITileEntityChangesColorsRainbow, ITickableTileEntity {
     private int colorChangeCounter = 0;
 
     public TileEntityRainbowBricks() {
@@ -40,10 +38,5 @@ public class TileEntityRainbowBricks extends TileEntity implements ITickableTile
 
     public void clicked(BlockState state, World worldIn, BlockPos pos) {
         updateColor(state, worldIn, pos);
-    }
-
-    private void updateColor(BlockState state, World worldIn, BlockPos pos) {
-        worldIn.setBlockState(pos,
-                state.cycle(BlockStateProperties.RAINBOW_COLORS));
     }
 }
